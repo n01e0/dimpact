acd3a8c76ed488d8b8bde70e5fd532a69b96626e
n01e0 <reoshiseki@gmail.com>
2026-03-06 23:03:39 +0900

    test(bench): add JavaScript heavy-diff fixture

---
+++ bench-fixtures/js-heavy/main.js
@@ -0,0 +1,182 @@
+function f00() {
+  const v = 0;
+  return v;
+}
+
+function f01() {
+  const v = f00();
+  const inc = 1;
+  return v + inc;
+}
+
+function f02() {
+  const v = f01();
+  const inc = 1;
+  return v + inc;
+}
+
+function f03() {
+  const v = f02();
+  const inc = 1;
+  return v + inc;
+}
+
+function f04() {
+  const v = f03();
+  const inc = 1;
+  return v + inc;
+}
+
+function f05() {
+  const v = f04();
+  const inc = 1;
+  return v + inc;
+}
+
+function f06() {
+  const v = f05();
+  const inc = 1;
+  return v + inc;
+}
+
+function f07() {
+  const v = f06();
+  const inc = 1;
+  return v + inc;
+}
+
+function f08() {
+  const v = f07();
+  const inc = 1;
+  return v + inc;
+}
+
+function f09() {
+  const v = f08();
+  const inc = 1;
+  return v + inc;
+}
+
+function f10() {
+  return f09() + 1;
+}
+
+function f11() {
+  return f10() + 1;
+}
+
+function f12() {
+  return f11() + 1;
+}
+
+function f13() {
+  return f12() + 1;
+}
+
+function f14() {
+  return f13() + 1;
+}
+
+function f15() {
+  return f14() + 1;
+}
+
+function f16() {
+  return f15() + 1;
+}
+
+function f17() {
+  return f16() + 1;
+}
+
+function f18() {
+  return f17() + 1;
+}
+
+function f19() {
+  return f18() + 1;
+}
+
+function f20() {
+  return f19() + 1;
+}
+
+function f21() {
+  return f20() + 1;
+}
+
+function f22() {
+  return f21() + 1;
+}
+
+function f23() {
+  return f22() + 1;
+}
+
+function f24() {
+  return f23() + 1;
+}
+
+function f25() {
+  return f24() + 1;
+}
+
+function f26() {
+  return f25() + 1;
+}
+
+function f27() {
+  return f26() + 1;
+}
+
+function f28() {
+  return f27() + 1;
+}
+
+function f29() {
+  return f28() + 1;
+}
+
+function f30() {
+  return f29() + 1;
+}
+
+function f31() {
+  return f30() + 1;
+}
+
+function f32() {
+  return f31() + 1;
+}
+
+function f33() {
+  return f32() + 1;
+}
+
+function f34() {
+  return f33() + 1;
+}
+
+function f35() {
+  return f34() + 1;
+}
+
+function f36() {
+  return f35() + 1;
+}
+
+function f37() {
+  return f36() + 1;
+}
+
+function f38() {
+  return f37() + 1;
+}
+
+function f39() {
+  return f38() + 1;
+}
+
+function main() {
+  return f39();
+}
