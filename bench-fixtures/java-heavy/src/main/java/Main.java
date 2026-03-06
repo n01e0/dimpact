3b1de860c0c0441915b59980dd41b2e386ff5b47
n01e0 <47088622+n01e0@users.noreply.github.com>
2026-03-06 19:34:12 +0900

    test(bench): add Java heavy-diff fixture for bench runs (#117)

---
+++ bench-fixtures/java-heavy/src/main/java/Main.java
@@ -0,0 +1,185 @@
+class Main {
+
+    static int m00() {
+        int v = 0;
+        return v;
+    }
+
+    static int m01() {
+        int v = m00();
+        int inc = 1;
+        return v + inc;
+    }
+
+    static int m02() {
+        int v = m01();
+        int inc = 1;
+        return v + inc;
+    }
+
+    static int m03() {
+        int v = m02();
+        int inc = 1;
+        return v + inc;
+    }
+
+    static int m04() {
+        int v = m03();
+        int inc = 1;
+        return v + inc;
+    }
+
+    static int m05() {
+        int v = m04();
+        int inc = 1;
+        return v + inc;
+    }
+
+    static int m06() {
+        int v = m05();
+        int inc = 1;
+        return v + inc;
+    }
+
+    static int m07() {
+        int v = m06();
+        int inc = 1;
+        return v + inc;
+    }
+
+    static int m08() {
+        int v = m07();
+        int inc = 1;
+        return v + inc;
+    }
+
+    static int m09() {
+        int v = m08();
+        int inc = 1;
+        return v + inc;
+    }
+
+    static int m10() {
+        return m09() + 1;
+    }
+
+    static int m11() {
+        return m10() + 1;
+    }
+
+    static int m12() {
+        return m11() + 1;
+    }
+
+    static int m13() {
+        return m12() + 1;
+    }
+
+    static int m14() {
+        return m13() + 1;
+    }
+
+    static int m15() {
+        return m14() + 1;
+    }
+
+    static int m16() {
+        return m15() + 1;
+    }
+
+    static int m17() {
+        return m16() + 1;
+    }
+
+    static int m18() {
+        return m17() + 1;
+    }
+
+    static int m19() {
+        return m18() + 1;
+    }
+
+    static int m20() {
+        return m19() + 1;
+    }
+
+    static int m21() {
+        return m20() + 1;
+    }
+
+    static int m22() {
+        return m21() + 1;
+    }
+
+    static int m23() {
+        return m22() + 1;
+    }
+
+    static int m24() {
+        return m23() + 1;
+    }
+
+    static int m25() {
+        return m24() + 1;
+    }
+
+    static int m26() {
+        return m25() + 1;
+    }
+
+    static int m27() {
+        return m26() + 1;
+    }
+
+    static int m28() {
+        return m27() + 1;
+    }
+
+    static int m29() {
+        return m28() + 1;
+    }
+
+    static int m30() {
+        return m29() + 1;
+    }
+
+    static int m31() {
+        return m30() + 1;
+    }
+
+    static int m32() {
+        return m31() + 1;
+    }
+
+    static int m33() {
+        return m32() + 1;
+    }
+
+    static int m34() {
+        return m33() + 1;
+    }
+
+    static int m35() {
+        return m34() + 1;
+    }
+
+    static int m36() {
+        return m35() + 1;
+    }
+
+    static int m37() {
+        return m36() + 1;
+    }
+
+    static int m38() {
+        return m37() + 1;
+    }
+
+    static int m39() {
+        return m38() + 1;
+    }
+
+    public static void main(String[] args) {
+        m39();
+    }
+}
