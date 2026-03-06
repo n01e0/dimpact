da39ac269a824c05ac2a3467be24760d883d3023
n01e0 <47088622+n01e0@users.noreply.github.com>
2026-03-06 23:07:53 +0900

    test(bench): add Ruby heavy-diff fixture (#149)

---
+++ bench-fixtures/ruby-heavy/main.rb
@@ -0,0 +1,182 @@
+def f00
+  v = 0
+  v
+end
+
+def f01
+  v = f00
+  inc = 1
+  v + inc
+end
+
+def f02
+  v = f01
+  inc = 1
+  v + inc
+end
+
+def f03
+  v = f02
+  inc = 1
+  v + inc
+end
+
+def f04
+  v = f03
+  inc = 1
+  v + inc
+end
+
+def f05
+  v = f04
+  inc = 1
+  v + inc
+end
+
+def f06
+  v = f05
+  inc = 1
+  v + inc
+end
+
+def f07
+  v = f06
+  inc = 1
+  v + inc
+end
+
+def f08
+  v = f07
+  inc = 1
+  v + inc
+end
+
+def f09
+  v = f08
+  inc = 1
+  v + inc
+end
+
+def f10
+  f09 + 1
+end
+
+def f11
+  f10 + 1
+end
+
+def f12
+  f11 + 1
+end
+
+def f13
+  f12 + 1
+end
+
+def f14
+  f13 + 1
+end
+
+def f15
+  f14 + 1
+end
+
+def f16
+  f15 + 1
+end
+
+def f17
+  f16 + 1
+end
+
+def f18
+  f17 + 1
+end
+
+def f19
+  f18 + 1
+end
+
+def f20
+  f19 + 1
+end
+
+def f21
+  f20 + 1
+end
+
+def f22
+  f21 + 1
+end
+
+def f23
+  f22 + 1
+end
+
+def f24
+  f23 + 1
+end
+
+def f25
+  f24 + 1
+end
+
+def f26
+  f25 + 1
+end
+
+def f27
+  f26 + 1
+end
+
+def f28
+  f27 + 1
+end
+
+def f29
+  f28 + 1
+end
+
+def f30
+  f29 + 1
+end
+
+def f31
+  f30 + 1
+end
+
+def f32
+  f31 + 1
+end
+
+def f33
+  f32 + 1
+end
+
+def f34
+  f33 + 1
+end
+
+def f35
+  f34 + 1
+end
+
+def f36
+  f35 + 1
+end
+
+def f37
+  f36 + 1
+end
+
+def f38
+  f37 + 1
+end
+
+def f39
+  f38 + 1
+end
+
+def main
+  f39
+end
