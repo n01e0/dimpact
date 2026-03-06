1912e6ae15c9015bf8947d94fb79d923bf0c32e0
n01e0 <47088622+n01e0@users.noreply.github.com>
2026-03-06 21:29:35 +0900

    ci(bench): add Python strict-LSP fixture and guardrail workflow job (#138)

---
+++ bench-fixtures/python-heavy/main.py
@@ -0,0 +1,141 @@
+def f00():
+    v = 0
+    return v
+
+def f01():
+    v = f00()
+    inc = 1
+    return v + inc
+
+def f02():
+    v = f01()
+    inc = 1
+    return v + inc
+
+def f03():
+    v = f02()
+    inc = 1
+    return v + inc
+
+def f04():
+    v = f03()
+    inc = 1
+    return v + inc
+
+def f05():
+    v = f04()
+    inc = 1
+    return v + inc
+
+def f06():
+    v = f05()
+    inc = 1
+    return v + inc
+
+def f07():
+    v = f06()
+    inc = 1
+    return v + inc
+
+def f08():
+    v = f07()
+    inc = 1
+    return v + inc
+
+def f09():
+    v = f08()
+    inc = 1
+    return v + inc
+
+def f10():
+    return f09() + 1
+
+def f11():
+    return f10() + 1
+
+def f12():
+    return f11() + 1
+
+def f13():
+    return f12() + 1
+
+def f14():
+    return f13() + 1
+
+def f15():
+    return f14() + 1
+
+def f16():
+    return f15() + 1
+
+def f17():
+    return f16() + 1
+
+def f18():
+    return f17() + 1
+
+def f19():
+    return f18() + 1
+
+def f20():
+    return f19() + 1
+
+def f21():
+    return f20() + 1
+
+def f22():
+    return f21() + 1
+
+def f23():
+    return f22() + 1
+
+def f24():
+    return f23() + 1
+
+def f25():
+    return f24() + 1
+
+def f26():
+    return f25() + 1
+
+def f27():
+    return f26() + 1
+
+def f28():
+    return f27() + 1
+
+def f29():
+    return f28() + 1
+
+def f30():
+    return f29() + 1
+
+def f31():
+    return f30() + 1
+
+def f32():
+    return f31() + 1
+
+def f33():
+    return f32() + 1
+
+def f34():
+    return f33() + 1
+
+def f35():
+    return f34() + 1
+
+def f36():
+    return f35() + 1
+
+def f37():
+    return f36() + 1
+
+def f38():
+    return f37() + 1
+
+def f39():
+    return f38() + 1
+
+def main():
+    return f39()
